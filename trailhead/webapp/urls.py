b1f55446e961367dd8932c683b263dc470d2e82b
Arizbeth Rojas <98544100+Arizbethrojas@users.noreply.github.com>
2024-10-18 14:32:41 -0400

    Backend hello world + navigation bar

---
+++ trailhead/webapp/urls.py
@@ -0,0 +1,8 @@
+#here is where we will place different URL routes and then connect them to our views 
+from django.urls import path 
+from . import views
+
+# list of our paths
+urlpatterns = [
+    path("", views.home, name ="home") #this is an empty path to the base URL of our website
+]
