85765731e970727b441b5a69507ff9bd209f5e17
colinkearns2 <colin.r.kearns.25@dartmouth.edu>
2024-10-22 22:13:47 -0400

    added database functionality

--- trailhead/webapp/models.py
+++ trailhead/webapp/models.py
@@ -8,3 +8,35 @@ from django.db import models
 class TodoItem(models.Model): 
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class Subclub(models.Model):
+    subclub_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.subclub_name
+
+class Trip(models.Model):
+    trip_name = models.CharField(max_length=255)
+    trip_date = models.DateField()
+    trip_description = models.TextField()
+    trip_leader = models.CharField(max_length=255)
+    trip_capacity = models.IntegerField()
+    subclub = models.ForeignKey(Subclub, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.trip_name
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.student_name
+
+class TripRegistration(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+
+class Waitlist(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
